6fd3b21089cab595fca4153612046053a3edb821
vinty1978@gmail.com <djkxtyjr3>
2018-04-05 13:18:14 +0300

    Специально допущена ошибка в тесте в PeopleTest

--- test/src/by/vinty/entity/PeopleTest.java
+++ test/src/by/vinty/entity/PeopleTest.java
@@ -30,6 +30,7 @@ public class PeopleTest {
         Map<String, People> map = new HashMap<>();
         Assertions.assertThat(map).isEmpty();
         Assertions.assertThat(map).hasSize(0);
+        Assertions.assertThat(map).hasSize(20);
         Assertions.assertThat(map).doesNotContainKey("x");
         assertThat(map.entrySet().iterator().hasNext()).isFalse();
         // или
