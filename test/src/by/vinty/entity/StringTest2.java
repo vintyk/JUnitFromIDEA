bd4d932972214cfee8c2e81c6a5edc39d1956c48
vinty1978@gmail.com <djkxtyjr3>
2018-04-05 12:07:46 +0300

    Все работает

--- test/src/by/vinty/entity/StringTest2.java
+++ test/src/by/vinty/entity/StringTest2.java
@@ -35,7 +35,6 @@ public class StringTest2 {
     public void testSubstr(){
         str1="llo";
         Assert.assertEquals(str1, str2.substring(2));
-        Assert.assertEquals(str1, str2.substring(1));
     }
 
     @After
